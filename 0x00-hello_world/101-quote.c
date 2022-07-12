cba5f40395765075c3ca973ab64c7f5b3509d852
Carlymax <ahmedkushyassir254@gmail.com>
2022-07-12 13:56:54 -0700

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints a line to the commandline using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints Programming is like building a multilingual puzzle and a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
